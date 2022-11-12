3f45e6b47bf5255aa846f6b334e470fd1d17d430
nokosse <kvisouth@e3r11p16.clusters.42paris.fr>
2022-11-12 12:00:05 +0100

    pas finis

---
+++ libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/12 11:47:26 by kvisouth          #+#    #+#             */
+/*   Updated: 2022/11/12 11:54:11 by kvisouth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+
+size_t	ft_strlen(const char *s);
+
+#endif
