53ab61d8f259be4b37bab5bd49e42873319e9c6e
nokosse <kvisouth@e3r11p16.clusters.42paris.fr>
2022-11-12 12:36:21 +0100

    fonctionnel mais doit ajouter ft_strlen

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,29 +6,29 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 16:03:25 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/11/12 11:22:07 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/11/12 12:35:18 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int	i;
+	size_t	i;
 
-	i = 0;
-	while (s[i])
-		i++;
-	while (s[i] != c)
-		i--;
-	if (s[0] != c)
+	i = strlen(s);
+
+	if (s[0] == '\0')
 		return (NULL);
+	while(s[i] != c && i != 0)
+		i--;
+	if (i == 0)
+		return (0);
 	return ((char *)(s + i));
 }
 
-/*
+#include <bsd/string.h>
+
 int	main(int ac, char **av)
 {
 
@@ -40,4 +40,3 @@ int	main(int ac, char **av)
 		printf("%s",strrchr(av[1],tofind));
 	}
 }
-*/
