db7f61daa082033fa561e352b2083790b0731192
nokosse <kvisouth@e3r3p17.clusters.42paris.fr>
2022-11-12 20:12:08 +0100

    fonctionnel & a la norme

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 16:03:25 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/11/12 12:35:18 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/11/12 20:11:42 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,25 @@ char	*ft_strrchr(const char *s, int c)
 {
 	size_t	i;
 
-	i = strlen(s);
-
+	i = ft_strlen(s);
 	if (s[0] == '\0')
 		return (NULL);
-	while(s[i] != c && i != 0)
+	while (s[i] != c && i != 0)
 		i--;
 	if (i == 0)
 		return (0);
 	return ((char *)(s + i));
 }
 
-#include <bsd/string.h>
+// #include <bsd/string.h>
+// int	main(int ac, char **av)
+// {
 
-int	main(int ac, char **av)
-{
-
-	int tofind = 'f';
-	if (ac == 2)
-	{
-		printf("%s",ft_strrchr(av[1],tofind));
-		printf("%s","\n");
-		printf("%s",strrchr(av[1],tofind));
-	}
-}
+// 	int tofind = 'f';
+// 	if (ac == 2)
+// 	{
+// 		printf("%s",ft_strrchr(av[1],tofind));
+// 		printf("%s","\n");
+// 		printf("%s",strrchr(av[1],tofind));
+// 	}
+// }
