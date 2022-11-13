6aa35c3d08f0ef9cd912e429be04cbedcf4ea75b
nokosse <kvisouth@e3r6p16.clusters.42paris.fr>
2022-11-13 15:59:13 +0100

    fonctionnel & a la norme

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,19 +6,19 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 16:37:23 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/11/12 11:24:20 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/11/13 15:58:32 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "libft.h"
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
 	i = 0;
+	if (s1[0] == '\0' && s2[0] == '\0')
+		return (0);
 	while (i < n)
 	{
 		if (s1[i] != s2[i])
@@ -28,14 +28,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	return (0);
 }
 
-/*
-int	main (int ac, char **av)
-{
-	if (ac == 4)
-	{
-		printf("%d",ft_strncmp(av[1], av[2], atoi(av[3])));
-		printf("%s","\n");
-		printf("%d",strncmp(av[1], av[2], atoi(av[3])));
-	}
-}
-*/
+// int	main (int ac, char **av)
+// {
+// 	if (ac == 4)
+// 	{
+// 		printf("%d",ft_strncmp(av[1], av[2], atoi(av[3])));
+// 		printf("%s","\n");
+// 		printf("%d",strncmp(av[1], av[2], atoi(av[3])));
+// 	}
+// }
