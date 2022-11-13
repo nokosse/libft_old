2cee011dd0d9c44149577a7f6ad127c83b80905a
nokosse <kvisouth@e3r6p16.clusters.42paris.fr>
2022-11-13 15:49:57 +0100

    fonctionnel & a la norme

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,25 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nok <nok@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 15:11:58 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/11/12 22:45:17 by nok              ###   ########.fr       */
+/*   Updated: 2022/11/13 15:49:32 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-
-size_t	ft_strlen(const char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
+#include "libft.h"
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
@@ -42,15 +31,13 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	return (ft_strlen(dest));
 }
 
-/*
-#include <bsd/string.h>
-int	main (int ac, char **av)
-{
-	if (ac == 7)
-	{
-		printf("%ld", ft_strlcat(av[1], av[2], atoi(av[3])));
-		printf("%s", "\n");
-		printf("%ld", strlcat(av[4], av[5], atoi(av[6])));
-	}
-}
-*/
+// #include <bsd/string.h>
+// int	main (int ac, char **av)
+// {
+// 	if (ac == 7)
+// 	{
+// 		printf("%ld", ft_strlcat(av[1], av[2], atoi(av[3])));
+// 		printf("%s", "\n");
+// 		printf("%ld", strlcat(av[4], av[5], atoi(av[6])));
+// 	}
+// }
