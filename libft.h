df0ad0341874d31e08e778dcb67559a54dabb19a
nokosse <kvisouth@e3r6p16.clusters.42paris.fr>
2022-11-13 16:06:24 +0100

    ajouter prototypes restants

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 11:47:26 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/11/12 19:37:05 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/11/13 16:06:03 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,23 @@
 # include <stdio.h>
 # include <string.h>
 
+int		ft_atoi(char *str);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+char	**ft_split(const char *s, char c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(const char *s1, const char *s2);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strrchr(const char *s, int c);
-
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 #endif
