df92e8006777064f81232c333e31dd1f5269a664
nokosse <kvisouth@e3r6p16.clusters.42paris.fr>
2022-11-13 17:23:14 +0100

    /

--- ft_memset.c
+++ ft_memset.c
@@ -3,23 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nok <nok@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 19:29:44 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/11/12 22:44:37 by nok              ###   ########.fr       */
+/*   Updated: 2022/11/13 17:22:59 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-
-// int	main(int ac, char **av)
-// {
-// 	(void) ac;
-// 	char *test = ["bonjour"];
-	
-// 	int val = 'X';
-// 	printf("%p",memset(av[1], val, atoi(av[2])));
-// }
--- ft_split.c
+++ ft_split.c
@@ -6,87 +6,13 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 15:50:04 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/11/13 15:42:37 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/11/13 17:22:58 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "libft.h"
 
-int	is_sep(const char letter, char c)
-{
-	if (letter == c)
-		return (1);
-	return (0);
-}
 
-size_t	ft_strlensplit(const char *s, char c)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0' && is_sep(s[i], c) == 0)
-		i++;
-	return (i);
-}
-
-size_t	strcnt(const char *s, char c)
-{
-	int	i;
-
-	i = 0; 
-	while (s[i])
-	{
-		if (is_sep(*s, c) == 0)
-			i++;
-	}
-	return (i);
-}
-
-char	*word(const char *s, char c)
-{
-	int		wdlen;
-	char	*wd;
-	int		i;
-
-	i = 0;
-	wdlen = ft_strlensplit (s, c);
-	wd = malloc (wdlen + 1);
-	if (!wd)
-		return (NULL);
-	while (i < wdlen)
-	{
-		wd[i] = s[i];
-		i++;
-	}
-	wd[i] = '\0';
-	return (wd);
-}
-
-char	**ft_split(const char *s, char c)
-{
-	char	**strs;
-	int		i;
-
-	strs = malloc (strcnt (s, c) + 1);
-	if (!strs)
-		return (NULL);
-	i = 0;
-	while (*s != '\0')
-	{
-		while (is_sep(*s, c) == 1 && *s)
-			s++;
-		if (*s)
-		{
-			strs[i] = word(s, c);
-			i++;
-		}
-		while (is_sep(*s, c) == 0 && *s)
-			s++;
-	}
-	strs[i] = NULL;
-	return (strs);
-}
 
 int	main (int ac, char **av)
 {
@@ -98,18 +24,8 @@ int	main (int ac, char **av)
 		while (split[i])
 		{
 			printf("%s\n",split[i]);
-//			printf("^ Adresse de i : %p",&i);
-//			printf("%s","\n");
 			i++;
 		}
-		free(split);
 
-		i = 0;
-		while(split[i])
-		{
-			free(split[i]);
-			i++;
-		}
-		free(split[i]);
 	}
 }
