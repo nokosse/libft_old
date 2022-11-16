84f5d7a129ee90c3bda958852fa38585b4211947
nokosse <visouthivong.kevin@gmail.com>
2022-11-16 17:38:58 +0100

    KO

---
+++ ft_substr.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/16 17:31:18 by kvisouth          #+#    #+#             */
+/*   Updated: 2022/11/16 17:32:37 by kvisouth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	(void)s;
+	(void)start;
+	(void)len;
+	return ((char *)s);
+}
