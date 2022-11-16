bb06528373fe9dda28728b857bb4a5e1c07d50be
nokosse <visouthivong.kevin@gmail.com>
2022-11-16 11:59:11 +0100

    ajout bzero

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 11:47:26 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/11/16 10:04:00 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/11/16 11:56:53 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,10 +45,10 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-// void	bzero(void *s, size_t n);
-// void	*memmove(void *dest, const void *src, size_t n);
-// void	*memchr(const void *s, int c, size_t n);
-// int		memcmp(const void *s1, const void *s2, size_t n);
+void	ft_bzero(void *s, size_t n);
+// void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+// int  ft_memcmp(const void *s1, const void *s2, size_t n);
 // void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 // char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 // char	*ft_strtrim(char const *s1, char const *set);
