6574adfe77229e3bacaf1e0961d853b31f50e6a0
nokosse <visouthivong.kevin@gmail.com>
2022-11-16 13:04:53 +0100

    fonctionnel & a la norme

---
+++ ft_calloc.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/16 12:01:59 by kvisouth          #+#    #+#             */
+/*   Updated: 2022/11/16 13:04:28 by kvisouth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+**	calloc alloue un bloc memoire de la taille de nmemb * size 
+**	puis initialise tout ces blocs alloués par des 0 (des '\0')
+*/
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*p;
+	int		len;
+
+	len = nmemb * size;
+	p = malloc(len);
+	if (!p)
+		return (NULL);
+	ft_bzero(p, len);
+	return (p);
+}
