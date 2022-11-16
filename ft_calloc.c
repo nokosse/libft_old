2b1ad34e76aa6c745ce55d417dd54924efe659d4
nokosse <visouthivong.kevin@gmail.com>
2022-11-16 13:07:48 +0100

    fonctionnel & a la norme

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 12:01:59 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/11/16 13:04:28 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/11/16 13:07:36 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 /*
 **	calloc alloue un bloc memoire de la taille de nmemb * size 
-**	puis initialise tout ces blocs alloués par des 0 (des '\0')
+**	et remplis ce bloc par des 0 (des '\0'), d'ou ft_bzero.
 */
 
 void	*ft_calloc(size_t nmemb, size_t size)
