62aa2ec78a74ae60ea1119dc970fce216fc83861
nokosse <visouthivong.kevin@gmail.com>
2022-11-16 12:38:24 +0100

    fonctionnel & a la norme

--- ft_split.c
+++ ft_split.c
@@ -6,30 +6,46 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 15:50:04 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/11/15 10:52:34 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/11/16 12:37:54 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	is_sep(const char s, char c) //Prend en parametre une lettre et la compare a c pour voir si c'est un separateur (1)
+/*
+**	Prend en parametre une lettre et la compare a char c 
+**	pour voir si c'est un separateur (1)
+*/
+
+int	is_sep(const char s, char c)
 {
 	if (s == c)
 		return (1);
 	return (0);
 }
 
-size_t	wrd_len(const char *s, char c) //Retourne en int la longueur d'un mot. (C'est un strlen special split)
+/*
+**  Retourne en int la longueur d'un mot.
+**	Parcours le mot et s'arrete des qu'il y a un separateur
+**	Enfait c'est un strnlen, ou n est le separateur.
+*/
+
+size_t	wrd_len(const char *s, char c)
 {
 	int	i;
 
 	i = 0;
-	while (s[i] != '\0' && is_sep(s[i], c) == 0) //Parcours le mot et s'arrete des qu'il y a un separateur
+	while (s[i] != '\0' && is_sep(s[i], c) == 0)
 		i++;
 	return (i);
 }
 
-size_t	strcnt(const char *s, char c) //Parcours toute la chaine *s, retourne sa longueur sans les separateurs
+/*
+** C'est un strlen mais qui ne compte pas les separateurs.
+** Compte les separateurs et strlen puis retourne strlen - separateurs.
+*/
+
+size_t	strcnt(const char *s, char c)
 {
 	int	i;
 	int	sep_count;
@@ -38,14 +54,19 @@ size_t	strcnt(const char *s, char c) //Parcours toute la chaine *s, retourne sa 
 	i = 0;
 	while (s[i])
 	{
-		if (is_sep(s[i], c) == 1)	//Compte les separateur
+		if (is_sep(s[i], c) == 1)
 			sep_count++;
-		i++;						//Compte len de *s
+		i++;
 	}
-	return (i - sep_count);			//Renvoie len - separateur
+	return (i - sep_count);
 }
 
-char	*word(const char *s, char c) //Fonctionne comme strdup. Mais jusqua wdlen, et non '\0'
+/*
+**	Fonctionne comme strdup mais jusqua wdlen et non '\0'
+**	Enfait c'est un strndup, ou n est le separateur, encore. 
+*/
+
+char	*word(const char *s, char c)
 {
 	int		wdlen;
 	char	*wd;
@@ -65,28 +86,45 @@ char	*word(const char *s, char c) //Fonctionne comme strdup. Mais jusqua wdlen, 
 	return (wd);
 }
 
+/*
+**	En gros le split comment il fonctionne:
+**	1. Il malloc la taille du tableau de tableau grace a notre
+**		fonction strcnt plus haut.
+**		On precise sizeof char car nos tableau de tableau seront des *char.
+**	2. Il parcours la chaine *s jusqua '\0', la base.
+**	3. Dans le while, il check si on est sur un separateur ou sur '\0'
+**		si oui il avance dans *s en incrementant le pointeur s.
+**	4. vu qu'on est plus sur un separateur grace au if precedent
+**		On est forcement sur un mot. Donc on strndup ce mot
+**		dans le 1er tableau de **split.
+**		Puis on passe au prochain tableau de **split en incrementant i.
+**	5. vu qu'on a deja strndup notre mot, il nous interesse plus
+**		donc on incremente s jusqu'a etre sur un separateur ou '\0'
+**	6. Enfin, il sort de la boucle et met NULL sur le dernier tab de tab.
+*/
+
 char	**ft_split(const char *s, char c)
 {
 	char	**strs;
 	int		i;
 
-	strs = malloc ((strcnt (s, c)) * (sizeof(char *)) + 1); //Le sizeof est important car il faut preciser que le tableau de tableau sera de type char*
+	strs = malloc ((strcnt (s, c)) * (sizeof(char *)) + 1);
 	if (!strs)
 		return (NULL);
 	i = 0;
 	while (*s != '\0')
 	{
-		while (is_sep(*s, c) == 1 && *s) // 1. Parcourir *s tant qu'on est sur un separateur
-			s++;	//On incremente le pointeur s. Il pointera donc sur le deuxieme caractere de *s, puis le troisieme etc..
-		if (*s)		//Si *s pas fini
+		while (is_sep(*s, c) == 1 && *s)
+			s++;
+		if (*s)
 		{
-			strs[i] = word(s, c);	//On envoi a word (strdup modifier) le pointeur s et la lettre separatrice.
-			i++;	//Une fois le mot mis dans le premier tableau de tableau, on incremente i pour passer au prochain tab de tab.
+			strs[i] = word(s, c);
+			i++;
 		}
-		while (is_sep(*s, c) == 0 && *s) //Vu quon a mis le mot qui nous interessais, on peut le skip et passer au mot suivamt.
-			s++;	//On incremente le pointeur s jusqu'a atteindre un separateur.
-	}				//Puis on remonte dans la boucle, ou on sera sur un separateur, ou la fin et on repete
-	strs[i] = NULL;	//On finis par mettre un NULL comme le demande a la consigne.
+		while (is_sep(*s, c) == 0 && *s)
+			s++;
+	}
+	strs[i] = NULL;
 	return (strs);
 }
 
