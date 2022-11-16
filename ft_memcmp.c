c869e516b47075c53e80170d090dc1f84602e2ea
nokosse <visouthivong.kevin@gmail.com>
2022-11-16 13:36:59 +0100

    KO

---
+++ ft_memcmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/16 13:19:05 by kvisouth          #+#    #+#             */
+/*   Updated: 2022/11/16 13:36:35 by kvisouth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const char	*str1;
+	const char	*str2;
+	size_t		i;
+
+	str1 = (const char *)s1;
+	str2 = (const char *)s2;
+	i = 0;
+	// if (str1[0] == '\0' && str2[0] == '\0')
+	// 	return (0);
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
+}
