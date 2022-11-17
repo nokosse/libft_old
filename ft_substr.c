000992bbb2427f43e1483419834e4f4083ec77af
nokosse <visouthivong.kevin@gmail.com>
2022-11-17 13:36:06 +0100

    fonctionnel & a la norme

--- ft_substr.c
+++ ft_substr.c
@@ -6,26 +6,22 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 17:31:18 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/11/16 19:56:16 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/11/17 13:35:44 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-**	B O N J O U R \0
-**	0 1 2 3 4 5 6 7 
-**	  ^         ^
-**	 start      len
-**	nb = 5 (O N J O U R)
+** ft_substr retourne une chaine
 */
 
 int	get_len(const char *s, unsigned int start, size_t len)
 {
-	int	nb;
+	int		nb;
 	size_t	max_len;
 
-	if(s[0] == '\0')
+	if (s[0] == '\0')
 		return (0);
 	max_len = strlen(s) - 1;
 	if (len > max_len)
@@ -40,32 +36,44 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
 	char			*str;
 	int				nb;
-	int	i;
-	int	j;
-	int	x;
+	int				i;
+	int				j;
+	int				x;
 
-	if(s[0] == '\0')
+	if (s[0] == '\0')
 		return (0);
 	nb = get_len(s, start, len);
 	str = malloc((sizeof(char *) * nb) + 1);
 	i = 0;
 	j = start;
 	x = 0;
-	while (x < nb)
+	while (x <= nb)
 	{
 		str[i] = s[j];
 		j++;
 		i++;
 		x++;
 	}
+	str[i] = '\0';
 	return (str);
 }
 
-int	main(void)
-{
+/*
+**	petit exemple :
+**	B O N J O U R \0
+**	0 1 2 3 4 5 6 7 
+**	  ^         ^
+**	 start      len
+**	$> ONJOUR
+*/
 
-		unsigned int start = 1;
-		size_t	size = 6;
-		const char str[] = "BONJOUR";
-		printf("%s",ft_substr(str, start, size));
-}
+// int	main(int ac, char **av)
+// {
+// 	if (ac == 4)
+// 	{
+// 		unsigned int start = atoi(av[2]);
+// 		size_t	size = atoi(av[3]);
+// 		const char *str = av[1];
+// 		printf("%s",ft_substr(str, start, size));
+// 	}
+// }
