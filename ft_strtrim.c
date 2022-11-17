0d32e25b94d24cf9ab334afb0e165a5770f052c4
nokosse <visouthivong.kevin@gmail.com>
2022-11-17 20:09:38 +0100

    fonctionnel & a la norme

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 17:29:29 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/11/17 20:04:40 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/11/17 20:09:17 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 **	*s1 = "+-++yo+les-mec--+-"
 **	*set= "+-"
 **	ft_strtrim(s1, set) = yo+les-mec
+**	enfait il enleve les char de *set a gauche et a droite seulement.
 */
 
 int	is_trim(const char letter, const char *set)
@@ -40,7 +41,7 @@ int	is_trim(const char letter, const char *set)
 int	left_trim(const char *s1, const char *set)
 {
 	int	i;
-	
+
 	i = 0;
 	while (is_trim(s1[i], set) == 1)
 		i++;
@@ -54,7 +55,7 @@ int	left_trim(const char *s1, const char *set)
 int	right_trim(const char *s1, const char *set)
 {
 	int	i;
-	
+
 	if (s1[0] == '\0')
 		return (0);
 	i = strlen(s1) - 1;
@@ -65,7 +66,6 @@ int	right_trim(const char *s1, const char *set)
 
 char	*ft_strtrim(const char *s1, const char *set)
 {
-	printf("strlen de s1: %ld\n",strlen(s1));
 	char	*str;
 	int		i;
 	int		j;
@@ -73,19 +73,15 @@ char	*ft_strtrim(const char *s1, const char *set)
 	int		max;
 
 	i = left_trim(s1, set);
-	printf("left  trim :%d\n",i);
 	j = i;
 	max = right_trim(s1, set) + 1;
-	printf("right trim :%d\n",max);
 	while (i != max)
 	{
 		i++;
 		n++;
 	}
 	str = malloc((n + 1) * (sizeof(char *)));
-	printf("malloc :%d * (sizeof(char *))\n\n",n + 1);
 	i = 0;
-	printf("j :%d\n",i);
 	while (j < max)
 	{
 		str[i] = s1[j];
@@ -96,12 +92,12 @@ char	*ft_strtrim(const char *s1, const char *set)
 	return (str);
 }
 
-int	main (int ac, char **av)
-{
-	if (ac == 3)
-	{
-	char *s1 = av[1];
-	char *set = av[2];
-	printf("Voici le trimage: %s",ft_strtrim(s1, set));
-	}
-}
+// int	main (int ac, char **av)
+// {
+// 	if (ac == 3)
+// 	{
+// 	char *s1 = av[1];
+// 	char *set = av[2];
+// 	printf("Voici le trimage: %s",ft_strtrim(s1, set));
+// 	}
+// }
