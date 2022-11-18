883635757ac855a1ceac7b3b058efbebe7eecada
nokosse <visouthivong.kevin@gmail.com>
2022-11-18 19:53:25 +0100

    running + norm [TEST : OK]

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 11:40:30 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/11/18 11:31:14 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/11/18 19:52:52 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,20 @@
 /*
 **	Cherche c dans *s, puis quand il le trouve, retourne un pointeur
 **	sur c dans *s, a la premiere occurence. 
+**	Selon le man, le caractere c signifie "octet" et ne fonctionnera pas
+**	avec un caractere multi-octet, en gros, c est un isprint. Je crois.
 */
 
 char	*ft_strchr(const char *s, int c)
 {
-	int	i;
-
-	i = 0;
-	while (s[i])
+	while (*s != '\0')
 	{
-		if (s[i] == c)
-			return ((char *)(s + i));
-		i++;
+		if ((unsigned char)*s == (unsigned char)c)
+			return ((char *)s);
+		s++;
 	}
+	if (c == 0)
+		return ((char *)s);
 	return (0);
 }
 
@@ -35,9 +36,10 @@ char	*ft_strchr(const char *s, int c)
 // {
 // 	if (ac == 2)
 // 	{
-// 		int tofind = 'j';
-// 		printf("%s", ft_strchr(av[1],tofind));
-// 		printf("%s", "\n");
-// 		printf("%s", strchr(av[1],tofind));
+// 		int tofind = 0;
+// 		printf("tofind :%d\n", tofind);
+// 		printf("mon strchr :%s", ft_strchr(av[1],tofind));
+// 		printf("%s","%\n");
+// 		printf("l'original :%s", strchr(av[1],tofind));
 // 	}
 // }
