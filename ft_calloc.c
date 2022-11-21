53c078377ef91702645b756af049027c8f3eceb5
nokosse <visouthivong.kevin@gmail.com>
2022-11-21 18:35:42 +0100

    running + norm [TEST : OK]

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 12:01:59 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/11/18 19:55:54 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/11/21 18:35:08 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 /*
 **	calloc alloue un bloc memoire de la taille de nmemb * size 
 **	et remplis ce bloc par des 0 (des '\0'), d'ou ft_bzero.
+**	/!\ Retourne NULL si on essaie d'allouer SIZE_MAX * SIZE_MAX
+**	et si l'allocation echoue.
 */
 
 void	*ft_calloc(size_t nmemb, size_t size)
@@ -22,6 +24,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*p;
 	int		len;
 
+	if (nmemb == SIZE_MAX && size == SIZE_MAX)
+		return (NULL);
 	len = nmemb * size;
 	p = malloc(len);
 	if (!p)
