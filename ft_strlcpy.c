408ed0a62f56953419e027bb8db0b4ed227b09bc
nokosse <visouthivong.kevin@gmail.com>
2022-11-22 13:45:43 +0100

    running + norm [TEST : OK]

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,38 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nok <nok@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 19:29:39 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/11/15 23:13:48 by nok              ###   ########.fr       */
+/*   Updated: 2022/11/22 13:45:01 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+**	Copie size caracteres de *src dans *dest
+**	Puis termine *dest par un '\0'
+*/
+
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	size_t	src_len;
 	size_t	i;
 
-	src_len = ft_strlen(src);
 	i = 0;
-	while (src[i] && i < size)
+	while (src[i] && size > 1)
 	{
 		dest[i] = src[i];
 		i++;
+		size--;
 	}
-	dest[i] = '\0';
-	return (src_len);
+	if (size > 0)
+		dest[i] = '\0';
+	return (ft_strlen(src));
 }
 
 // #include <bsd/string.h>
 // int	main (int ac, char **av)
 // {
-// 	if (ac == 7)
+// 	if (ac == 4)
 // 	{
-// 		printf("%ld",ft_strlcpy(av[1], av[2], atoi(av[3])));
+// 		printf("%ld",strlcpy(av[1], av[2], atoi(av[3])));
 // 		printf("%s","\n");
-// 		printf("%ld",strlcpy(av[4], av[5], atoi(av[6])));	
+// 		//printf("%ld",strlcpy(av[4], av[5], atoi(av[6])));	
 // 	}
 // }
