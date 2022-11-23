fb0d23d82989be6cf242aa8e6b162f193eca1167
nokosse <visouthivong.kevin@gmail.com>
2022-11-23 11:52:14 +0100

    running + norm [TEST : OK]

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,14 +6,28 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 17:23:16 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/11/17 20:12:18 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/11/23 11:51:02 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+**	Applique la fonction ’f’ à chaque caractère de la
+**	chaîne de caractères transmise comme argument,
+**	et en passant son index comme premier argument.
+**	Chaque caractère est transmis par adresse à ’f’
+**	afin d’être modifié si nécessaire.
+*/
+
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	(void)s;
-	(void)f;
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		(*f)(i, s + i);
+		i++;
+	}
 }
