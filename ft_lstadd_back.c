ca0830bfbff00584037c3dca7f93d36f70615bdc
nokosse <visouthivong.kevin@gmail.com>
2022-11-26 18:59:09 +0100

    not tested + norm:OK

---
+++ ft_lstadd_back.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/26 17:46:33 by kvisouth          #+#    #+#             */
+/*   Updated: 2022/11/26 17:48:21 by kvisouth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+**	Ajoute l’élément ’new’ à la fin de la liste.
+**	lst : adresse du pointeur vers le premier elem. de la liste.
+**	new : adresse du pointeur vers l'element a rajouter dans la liste.
+*/
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	
+}
