40d755eefa1c9e07c02a0f757b2563fc19f0351f
nokosse <visouthivong.kevin@gmail.com>
2022-11-28 13:29:01 +0100

    not tested + norm:OK

---
+++ ft_lstiter.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/28 13:21:47 by kvisouth          #+#    #+#             */
+/*   Updated: 2022/11/28 13:28:47 by kvisouth         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+**	Itère sur la liste *lst et applique la fonction f au contenu chaque élément.
+**	lst : L’adresse du pointeur vers un élément.
+**	f : L’adresse de la fonction à appliquer.
+*/
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*temp;
+
+	temp = lst;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		(*f)(temp->content);
+	}
+}
