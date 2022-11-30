0fa8614ac99869e6a6732d76429509c6babeacb6
root <root@LAPTOP-3PG926D4.localdomain>
2022-11-30 13:48:17 +0000

    test:OK + norm:PAS SUR

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 16:37:23 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/11/29 11:58:35 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/11/30 13:47:44 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (s1[0] == '\0' && s2[0] == '\0')
-		return (0);
 	while (i < n)
 	{
 		if (s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
-		if (s1[i] == '\0' || s2[i] == '\0')
-			return (0);
 	}
 	return (0);
 }
