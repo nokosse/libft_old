0dd4d4358fb0e31fa4bfe2dbfec2e8b4a3663255
nokosse <visouthivong.kevin@gmail.com>
2022-11-30 18:06:30 +0100

    test:OK + norm:OK

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,9 +6,14 @@
 /*   By: kvisouth <kvisouth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 19:42:52 by kvisouth          #+#    #+#             */
-/*   Updated: 2022/11/23 13:45:19 by kvisouth         ###   ########.fr       */
+/*   Updated: 2022/11/30 18:01:49 by kvisouth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+**	Un 'file descriptor' (fd) c'est un INT a bas niveau utiliser pour identifier
+**	un fichier ouvert au niveau du Kernel.
+*/
 
 #include "libft.h"
 
